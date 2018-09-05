3aaf515dd53167a0df44f54eabbef819bf8f013a
iOSDev <271901088@qq.com>
2018-09-05 17:14:16 +0800

    测试cocoapods

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asdf
+//
+//  Created by mac on 22/05/2017.
+//  Copyright © 2017 MCIphoneDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
